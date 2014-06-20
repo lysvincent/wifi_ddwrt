62956a293854cf81e3c46e08ff216bf818086c2c
YungShen.Lew <yung.shen.lew@ipa.fraunhofer.de>
2014-06-20 14:49:10 +0200

    latest

---
+++ scripts/opendict.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import sys,string
+import rospy
+import roslaunch
+from wifi_ddwrt.msg import *
+import math
+import tf
+import pickle
+from geometry_msgs.msg import *
+from nav_msgs.srv import *
+from visualization_msgs.msg import *
+from StringIO import StringIO
+
+dic = pickle.load(open("fingerprints.pkl","rb"))
+
+print dic.keys(), "=", type(dic.keys())
+a = dic.keys()
+b = a[0]
+print "a=", type(a[0])
+print "a=", type(list(a[0]))
+print "b=", a[0]
+print "b[0]=", a[0][0]
+
+print "dic(b)=", dic[b], "is", type(dic[b])
+
+inv_fp = {v:k for k, v in dic.items()}
+fp_keys=list(inv_fp.keys())
+print "rev_dic.keys=", fp_keys
+print "first ap of the first key=", fp_keys[0][0]
+print "the ssid=", fp_keys[0][0][0]
